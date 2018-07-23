34887753187e0c768860491a4879abbbb6ef1f31
widelec-BB <5738944+widelec-BB@users.noreply.github.com>
2018-07-23 16:08:33 +0200

    Remove old authorized key and bump version number

--- provision/setup_base.sh
+++ provision/setup_base.sh
@@ -4,10 +4,10 @@ echo "-------------- SETUP BASE ------------------"
 echo "---------------------------------------------"
 
 sudo rm -rf /etc/motd
-echo -e "--\nwidelec-BB webdev VM version 0.1.0\n--" | sudo tee /etc/motd > /dev/null
+echo -e "--\nwidelec-BB webdev VM version 0.1.1\n--" | sudo tee /etc/motd > /dev/null
 echo -e "webdev-vm" | sudo tee /etc/hostname >/dev/null
-sudo sed -i '/127.0.1.1/c\127.0.1.1  webdev-vm.localdomain  webdev-vm' /etc/hosts
+sudo sed -i '/127.0.1.1/c\127.0.1.1  webdev-vm.local  webdev-vm' /etc/hosts
 
-wget -q -O - 'https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub' | sudo tee --append /home/vagrant/.ssh/authorized_keys >/dev/null
+wget -q -O - 'https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub' | sudo tee /home/vagrant/.ssh/authorized_keys >/dev/null
 
 sudo shutdown -r now
