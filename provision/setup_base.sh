3cc8a57cafb140b231987a6598f31ad640a02241
widelec-BB <5738944+widelec-BB@users.noreply.github.com>
2018-07-25 19:02:35 +0200

    Bump version number

--- provision/setup_base.sh
+++ provision/setup_base.sh
@@ -4,7 +4,7 @@ echo "-------------- SETUP BASE ------------------"
 echo "---------------------------------------------"
 
 sudo rm -rf /etc/motd
-echo -e "--\nwidelec-BB webdev VM version 0.1.1\n--" | sudo tee /etc/motd > /dev/null
+echo -e "--\nwidelec-BB webdev VM version 0.1.2\n--" | sudo tee /etc/motd > /dev/null
 echo -e "webdev-vm" | sudo tee /etc/hostname >/dev/null
 sudo sed -i '/127.0.1.1/c\127.0.1.1  webdev-vm.local  webdev-vm' /etc/hosts
 
