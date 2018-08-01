c4af11c4b26062b856710471b1f603b8ad7546c1
widelec-BB <5738944+widelec-BB@users.noreply.github.com>
2018-08-01 20:05:22 +0200

    Reboot before packaging

--- build.sh
+++ build.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+# remove old build
 rm -f package.box
 vagrant destroy -f || exit
+
+# create and provision
 vagrant up || exit
+
+# reboot for VBox additions
+vagrant halt || exit
+vagrant up || exit
+
+# stop and package
 vagrant halt || exit
 vagrant package || exit
 
