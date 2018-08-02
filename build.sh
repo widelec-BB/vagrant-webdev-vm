10cb1e14747e706d57c6a0940e8cfe4b1278537d
widelec-BB <5738944+widelec-BB@users.noreply.github.com>
2018-08-02 23:28:28 +0200

    Add explanation for reboot in build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,8 @@ vagrant destroy -f || exit
 # create and provision
 vagrant up || exit
 
-# reboot for VBox additions
+# reboot for VBox additions (apt-get upgrade in provision may cause
+# need for reinstallation of VBox additions)
 vagrant halt || exit
 vagrant up || exit
 
